33aa8aeeb23d5b2918985f9ed9405631fb3e17a0
tiedat003 <tiendat20031009@gmail.com>
2025-02-19 11:23:35 +0700

    thêm người dùng

--- users-add.php
+++ users-add.php
@@ -9,21 +9,63 @@
 <html lang="en">    
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Dashboard - Inventory Management System</title>
+        <title>Bảng điều khiển - Hệ thống quản lý hàng tồn kho</title>
         <link rel="stylesheet" href="css/login.css" type=" text/css">
         <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
     </head>
     <body>
         <div id="dashboardMainContainer">
             <?php include 'partials/app-sidebar.php'; ?>
-            <h1>Add User</h1>
-            <form action="database/add.php" method="POST">
-                <input type="text" name="first_name" placeholder="First Name" required>
-                <input type="text" name="last_name" placeholder="Last Name" required>
-                <input type="email" name="email" placeholder="Email" required>
-                <input type="password" name="password" placeholder="Password" required>
-                <button type="submit">Add User</button>
-            </form>
-            <a href="users.php">Back to Users</a>
+            <div class="dashboard_content_container" id="dashboard_content_container">
+                <?php include('partials/app-topnav.php') ?>
+                <div class="dashboard_content">
+                    <div class="dashboard_content-main">
+                        <div class="row">
+                            <div class="column-5">
+                                <h1 class="section_header">Thêm người dùng</h1>
+                                    <div id="userAddFormContainer">
+                                        <form action="database/add.php" method="POST" class="appForm">
+                                            <div class="appFormInputContainer">
+                                                <label for="first_name" >Họ</label>
+                                                <input type="text" name="first_name" id="first_name" class="appFormInput" required>
+                                            </div>
+                                            <div class="appFormInputContainer">
+                                                <label for="last_name" >Tên</label>
+                                                <input type="text" name="last_name" id="last_name" class="appFormInput" required>
+                                            </div>
+                                            <div class="appFormInputContainer">
+                                                <label for="email" >Email</label>
+                                                <input type="text" name="email" id="email" class="appFormInput" required>
+                                            </div> 
+                                            <div class="appFormInputContainer">
+                                                <label for="password" >Mật khẩu</label>
+                                                <input type="password" name="password" id="password" class="appFormInput" required>
+                                            </div>    
+                                            <button class="appBtn" type="submit"><i class="fa fa-plus"></i>Add User</button>
+                                        </form>
+                                        <?php if (isset($_SESSION['response'])) {
+                                            $response_message = $_SESSION['response']['message'];
+                                            $is_success = $_SESSION['response']['success'];
+                                        ?>
+                                            <div class="responseMessage">
+                                                <p class="responseMessage <?= $is_success ? 'responseMessage__success' : 'responseMessage__error' ?>">
+                                                    <?= $response_message ?>
+                                                </p>
+                                            </div>
+                                        <?php unset($_SESSION['response']); } ?>
+                                    </div>
+                                </div>  
+                            </div>
+                            <div class="column-7">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>     
+            </div>
         </div>
+
+
+        <script src="js/app.js"></script>
     </body>
+</html>
