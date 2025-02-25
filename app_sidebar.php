3cfba6065875a3a6b84f627b7526a7ea40b140c9
Triệu Minh Hiếu <tmhieu012@gmail.com>
2025-02-25 11:28:57 +0700

    update

--- app_sidebar.php
+++ app_sidebar.php
@@ -13,19 +13,4 @@
                         <a href=""><i class="fas fa-gauge"></i><span class="menuText"> Dashboard</span></a>
                     </li>
                 </ul>
-            </div><div class="dashboard_sidebar" id="dashboard_sidebar">
-            <h3 class="dashboard_logo" id="dashboard_logo">IMS</h3>
-            <div class="dashboard_sidebar_user">
-                <img src="images\user\DSCF1892.jpg" alt="User image." />
-                <span>Trung Hieu</span>
-            </div>
-            <div class="dashboard_sidebar_menus">
-                <ul class="dashboard_menu_lists">
-                    <li class="menuActive">
-                        <a href=""><i class="fas fa-gauge"></i><span class="menuText"> Dashboard</span></a>
-                    </li>
-                    <li>
-                        <a href=""><i class="fas fa-gauge"></i><span class="menuText"> Dashboard</span></a>
-                    </li>
-                </ul>
             </div>
