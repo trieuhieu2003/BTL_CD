5f1715d2090336ff59c6744db1ee30a4b0f4dd70
trungtan310103 <dotrungtan31@gmail.com>
2025-03-15 15:00:54 +0700

    quản lý đơn hàng 1

--- partials/app_slidebar.php
+++ partials/app_slidebar.php
@@ -16,37 +16,49 @@ $user = $_SESSION['user'];
             <li class="liMainMenu">
                 <a href="javascript:void(0);" class="showHideSubMenu">
                     <i class="fas fa-tag showHideSubMenu"></i>
-                    <span class="menuText showHideSubMenu"> Product Management </span>
+                    <span class="menuText showHideSubMenu"> Sản Phẩm </span>
                     <i class="fas fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
                 </a>
 
                 <ul class="subMenus" id="user">
-                    <li><a class="subMenuLink" href="./product-view.php"><i class="fas fa-circle"></i>view Product</a></li>
-                    <li><a class="subMenuLink" href="./product-add.php"><i class="fas fa-circle"></i>add Product</a></li>
+                    <li><a class="subMenuLink" href="./product-view.php"><i class="fas fa-circle"></i>Quản Lý Sản Phẩm</a></li>
+                    <li><a class="subMenuLink" href="./product-add.php"><i class="fas fa-circle"></i>Thêm Sản Phẩm</a></li>
                 </ul>
             </li>
             <li class="liMainMenu">
                 <a href="javascript:void(0);" class="showHideSubMenu">
                     <i class="fas fa-truck showHideSubMenu"></i>
-                    <span class="menuText showHideSubMenu"> Supplier Management </span>
+                    <span class="menuText showHideSubMenu"> Nhà Cung Cấp </span>
                     <i class="fas fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
                 </a>
 
                 <ul class="subMenus" id="user">
-                    <li><a class="subMenuLink" href=""><i class="fas fa-circle"></i>view Supplier</a></li>
-                    <li><a class="subMenuLink" href=""><i class="fas fa-circle"></i>add Supplier</a></li>
+                    <li><a class="subMenuLink" href=""><i class="fas fa-circle"></i>Quản Lý Nhà Cung Cấp</a></li>
+                    <li><a class="subMenuLink" href=""><i class="fas fa-circle"></i>Thêm Nhà Cung Cấp</a></li>
+                </ul>
+            </li>
+            <li class="liMainMenu">
+                <a href="javascript:void(0);" class="showHideSubMenu">
+                    <i class="fas fa-shopping-cart"></i>
+                    <span class="menuText showHideSubMenu">Đơn Hàng</span>
+                    <i class="fas fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
+                </a>
+                
+                <ul class="subMenus" id="user">
+                    <li><a class="subMenuLink" href="./view-order.php"><i class="fas fa-circle"></i>Quản Lý Đơn Hàng</a></li>
+                    <li><a class="subMenuLink" href="./product-order.php"><i class="fas fa-circle"></i>Tạo Đơn Hàng</a></li>
                 </ul>
             </li>
             <li class="liMainMenu showHideSubMenu">
                 <a href="javascript:void(0);" class="showHideSubMenu">
                     <i class="fas fa-user-plus showHideSubMenu"></i>
-                    <span class="menuText showHideSubMenu"> User Management </span>
+                    <span class="menuText showHideSubMenu"> Người Dùng </span>
                     <i class="fas fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
                 </a>
 
                 <ul class="subMenus" id="user">
-                    <li><a class="subMenuLink" href="./users_view.php"><i class="fas fa-circle"></i>view users</a></li>
-                    <li><a class="subMenuLink" href="./user_add.php"><i class="fas fa-circle"></i>add user</a></li>
+                    <li><a class="subMenuLink" href="./users_view.php"><i class="fas fa-circle"></i>Quản Lý Người Dùng</a></li>
+                    <li><a class="subMenuLink" href="./user_add.php"><i class="fas fa-circle"></i>Thêm Người Dùng</a></li>
                 </ul>
             </li>
 
